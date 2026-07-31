4e64061bdafbf5be1ab20d22b8a74337fc7edbc4
alsifatby <alsifat.by@gmail.com>
2026-07-31 20:59:43 +0600

    Update withdraw.php

--- modules/withdraw.php
+++ modules/withdraw.php
@@ -1,21 +1,8 @@
 <?php
 declare(strict_types=1);
-
-class WithdrawSystem {
-    private PDO $db;
-
-    public function __construct(PDO $db) {
-        $this->db = $db;
-    }
-
-    public function renderMethods(int|string $chat_id, int|string $user_id): void {
-        $text = "💳 <b>Withdrawal System</b>\n\nSelect your payment method:";
-        $keyboard = [
-            'inline_keyboard' => [
-                [['text' => 'Bkash / Nagad', 'callback_data' => 'wd_bkash'], ['text' => 'USDT / Crypto', 'callback_data' => 'wd_crypto']],
-                [['text' => '🔙 Back to Menu', 'callback_data' => 'menu_home']]
-            ]
-        ];
-        sendMessage($chat_id, $text, $keyboard);
-    }
+$methods = $settings['methods'] ?? ['bKash', 'Nagad'];
+$btns = []; 
+foreach($methods as $m) {
+    $btns[] = [['text' => "💳 $m", 'callback_data' => "wdm_$m"]];
 }
+send($uid, "💸 <b>Select Payout Method:</b>\n(Minimum Withdraw: {$settings['min_wd']} {$cur})", ['inline_keyboard' => $btns]);
